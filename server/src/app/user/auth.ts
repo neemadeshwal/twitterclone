d0c10bafc62646276e968c5a673cdd9d53957d6e
Neema deshwal <neemadeshwal.py@gmail.com>
2024-10-28 22:44:26 +0530

    some changes

--- server/src/app/user/auth.ts
+++ server/src/app/user/auth.ts
@@ -21,8 +21,10 @@ router.get(
   async (req: any, res) => {
     console.log(req.user);
 
-    const email = (req.user && req.user?.email) ?? "";
-    if (!email) {
+    const email = req.user && req.user.email;
+    console.log(email, "email");
+    if (email === "" || "undefined" || null) {
+      console.log("heyy try");
       throw new Error("no email please try again.");
     }
 
@@ -35,6 +37,37 @@ router.get(
       res.cookie("token", token, { httpOnly: true });
       res.redirect("http://localhost:5000");
     }
+    const firstName = req.user.name.givenName;
+    const lastName = req.user.name.lastName ?? "";
+    let userName;
+
+    const existingUserName = await prismaClient.user.findUnique({
+      where: {
+        userName: `@${lastName ?? "_"}${firstName}`,
+      },
+    });
+    if (existingUserName) {
+      userName = `@${existingUserName}${Math.floor(Math.random() * 20)}`;
+    } else {
+      userName = `@${lastName ?? "_"}${firstName}`;
+    }
+
+    const profileImgUrl =
+      req.user.photos.length !== 0 ? req.user.photos[0].value : "";
+
+    const newUser = await prismaClient.user.create({
+      data: {
+        email: email,
+        firstName,
+        lastName,
+        userName,
+        profileImgUrl,
+      },
+    });
+    const token = await JWTService.generateTokenFromUser(newUser);
+    console.log(token, "token");
+    res.cookie("token", token, { httpOnly: true });
+
     res.redirect("http://localhost:5000");
   }
 );
