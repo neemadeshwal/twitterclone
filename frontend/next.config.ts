aa846f8268a23588e5b36e113e1f696eed799159
Neema deshwal <neemadeshwal.py@gmail.com>
2025-03-03 17:42:44 +0530

    changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { SERVER_URL } from "./lib/constants";
 
 const nextConfig: NextConfig = {
   images: {
@@ -13,6 +14,31 @@ const nextConfig: NextConfig = {
       "media2.giphy.com",
       "media4.giphy.com"
     ], // Add your allowed domains here
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: SERVER_URL!, // Your backend URL
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization',
+          },
+        ],
+      },
+    ];
   },
 };
 
