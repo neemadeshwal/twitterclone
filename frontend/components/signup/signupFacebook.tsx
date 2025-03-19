64d3cc5693a4e02cd09501f1f23ec801e5e62824
Neema Deshwal <neemadeshwal.py@gmail.com>
2025-03-19 23:44:12 +0530

    some chanfges

--- frontend/components/signup/signupFacebook.tsx
+++ frontend/components/signup/signupFacebook.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { FaFacebook } from "react-icons/fa";
 
@@ -6,11 +7,11 @@ const SignupFacebook = ({ signupType }: { signupType: "login" | "signup" }) => {
     <div>
       <div>
         <button
-         onClick={() =>
-           
-          (window.location.href = `${process.env. NEXT_PUBLIC_GOOGLE_URL}`)
-        }
-        className="bg-white hover:bg-[#ffffffed] text-black py-2 w-full items-center rounded-full flex justify-center gap-2 ">
+          onClick={() =>
+            (window.location.href = `${process.env.NEXT_PUBLIC_GOOGLE_URL}`)
+          }
+          className="bg-white hover:bg-[#ffffffed] text-black py-2 w-full items-center rounded-full flex justify-center gap-2 "
+        >
           <FaFacebook className="text-[25px] text-[#1877F2]" />
           <h6 className="text-[15px] font-[600] text-[#111111]">
             Sign {signupType === "login" ? "in" : "up"} with facebook
